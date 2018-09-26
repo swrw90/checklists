03685fdacda279ac56d2d748f995c2387e16ed59
Seth Watson <seth.watson1@gmail.com>
2018-09-26 13:09:37 -0600

    Removing unnecessary comments

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -16,11 +16,8 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
         title = checklist.name
-        
-//        print("Documents folder is \(documentsDirectory())")
-//        print("Data file path is \(dataFilePath())")
+
     }
     
     override func tableView(_ tableView: UITableView,
@@ -69,44 +66,8 @@ class ChecklistViewController: UITableViewController {
     func configureText(for cell: UITableViewCell,
                           with item: ChecklistItem) {
         let label = cell.viewWithTag(1000) as! UILabel
-        //label.text = item.text
         label.text = "\(item.itemID): \(item.text)"
     }
-    
-//    func documentsDirectory() -> URL {
-//        let paths = FileManager.default.urls(for: .documentDirectory,
-//                                             in: .userDomainMask)
-//        return paths[0]
-//    }
-//
-//    func dataFilePath() -> URL {
-//        return documentsDirectory().appendingPathComponent(
-//            "Checklists.plist")
-//    }
-    
-//    func saveChecklistItems() {
-//        let encoder = PropertyListEncoder()
-//        do {
-//            let data = try encoder.encode(items)
-//            try data.write(to: dataFilePath(),
-//                           options: Data.WritingOptions.atomic)
-//        } catch {
-//            print("Error encoding item array!")
-//        }
-//    }
-//
-//    func loadChecklistItems() {
-//        let path = dataFilePath()
-//        if let data = try? Data(contentsOf: path) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                items = try decoder.decode([ChecklistItem].self,
-//                                           from: data)
-//            } catch {
-//                print("Error decoding item array!")
-//            }
-//        }
-//    }
     
     override func tableView(
         _ tableView: UITableView,
